9000603e6044d77f774b8e2eac865aa0f1fd946f
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-03-27 21:52:04 +0300

    Tree traversal

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,20 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root=None):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    def get_element_by_id(self, target_id):
+        return self._dfs(self.root, target_id)
+
+    def _dfs(self, node, target_id):
+        if node is None:
+            return None
+
+        if node.get('id') == target_id:
+            return node
+
+        for child in node.get('children', []):
+            found_node = self._dfs(child, target_id)
+            if found_node:
+                return found_node
+
+        return None
